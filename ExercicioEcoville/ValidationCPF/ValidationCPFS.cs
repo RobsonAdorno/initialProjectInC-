98ce68ad8a1ba0320c36dfb79bbee7eb672bdf07
Robson Adorno <rferreira@scopus.com.br>
2018-03-06 10:47:59 -0300

    Validando CPF

---
+++ ExercicioEcoville/ValidationCPF/ValidationCPFS.cs
@@ -0,0 +1,80 @@
+﻿using System;
+namespace ExercicioEcoville.ValidationCPF
+{
+    public class ValidationCPFS
+    {
+        public bool Valida(string cpf)
+        {
+            int[] multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            int[] multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+            string tempCpf;
+            string digito;
+            string dgsVerificadores;
+            int soma;
+            int resto;
+            cpf = cpf.Trim();
+            cpf = cpf.Replace(".", "").Replace("-", "");
+            if ((cpf == "00000000000") ||
+                (cpf == "11111111111") ||
+                (cpf == "22222222222") ||
+                (cpf == "33333333333") ||
+                (cpf == "44444444444") ||
+                (cpf == "55555555555") ||
+                (cpf == "66666666666") ||
+                (cpf == "77777777777") ||
+                (cpf == "88888888888") ||
+                (cpf == "99999999999"))
+            {
+                return false;
+            }
+            if (cpf.Length != 11)
+
+                return false;
+
+            tempCpf = cpf.Substring(0, 9);
+            soma = 0;
+            for (int i = 0; i < 9; i++)
+
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+            resto = soma % 11;
+
+            if (resto < 2)
+
+                resto = 0;
+
+            else
+
+                resto = 11 - resto;
+
+            digito = resto.ToString();
+            dgsVerificadores = digito;
+            tempCpf = tempCpf + digito;
+            soma = 0;
+            for (int i = 0; i < 10; i++)
+
+                soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+            resto = soma % 11;
+
+            if (resto < 2)
+
+                resto = 0;
+
+            else
+
+                resto = 11 - resto;
+
+            digito = resto.ToString();
+            dgsVerificadores += digito;
+
+            if ((dgsVerificadores) == cpf.Remove(0, 9))
+            {
+                return true;
+            }
+            else
+            {
+                Console.WriteLine("CPF Inválido!!!");
+                return false;
+            }
+        }
+    }
+}
---
+++ ExercicioEcoville/View(Main)/Program.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using ExercicioEcoville.Model;
+using ExercicioEcoville.ValidationCPF;
+
+namespace ExercicioEcoville
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClasseDeModelo CM = new ClasseDeModelo();
+            ValidationCPFS VC = new ValidationCPFS();
+            string op;
+            string opcao;
+            do
+            {
+                Console.WriteLine("Selecione a opção desejada:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("1 - Cadastrar Cliente:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("2 - Listar Clientes:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("3 - Cadastrar Vendedor:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("4 - Listar Vendedores:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("5 - Cadastrar Produtos:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("6 - Listar Produtos:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("7 - Registrar Venda:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("8 - Listar Venda:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("9 - Listar Vendas por Cliente:");
+                Console.WriteLine("=============================");
+                Console.WriteLine("0 - Sair:");
+                op = Console.ReadLine();
+                opcao = "N";
+
+                switch (op)
+                {
+                    case "1":
+                        Console.WriteLine("Digite o nome, por favor!");
+                        CM.nome = Console.ReadLine();
+
+                        Console.WriteLine("Digite o cpf, por favor!");
+                        CM.cpf = Console.ReadLine();
+                        if (VC.Valida(CM.cpf) == false)
+                        {
+                            Console.WriteLine("CPF INVÁLIDO!");
+                        }
+                        else
+                        {
+                            
+                            Console.WriteLine("Deseja sair? [S/N]");
+                            opcao = Console.ReadLine();
+
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Nome do cliente é : " + CM.nome);
+                        break;
+
+                    default:
+                        Console.WriteLine("Encerrando a aplicação...");
+                        Environment.Exit(0);
+                        break;
+                }
+
+
+            } while (opcao.Equals("N"));
+            Console.WriteLine("\nAperte uma tecla para continuar...");
+            Console.ReadKey();
+
+            Console.WriteLine("Selecione a opção desejada:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("1 - Cadastrar Cliente:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("2 - Listar Clientes:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("3 - Cadastrar Vendedor:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("4 - Listar Vendedores:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("5 - Cadastrar Produtos:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("6 - Listar Produtos:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("7 - Registrar Venda:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("8 - Listar Venda:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("9 - Listar Vendas por Cliente:");
+            Console.WriteLine("=============================");
+            Console.WriteLine("0 - Sair:");
+            op = Console.ReadLine();
+            opcao = "N";
+
+            switch (op)
+            {
+                case "1":
+                    Console.WriteLine("Digite o nome, por favor!");
+                    CM.nome = Console.ReadLine();
+                    Console.WriteLine("Digite o cpf, por favor!");
+                    CM.cpf = Console.ReadLine();
+                    VC.Valida(CM.cpf);
+                    Console.WriteLine("Deseja sair? [S/N]");
+                    opcao = Console.ReadLine();
+                    break;
+                case "2":
+                    Console.WriteLine("Nome do cliente é : " + CM.nome);
+                    break;
+
+                default:
+                    Console.WriteLine("Encerrando a aplicação...");
+                    Environment.Exit(0);
+                    break;
+            }
+
+        }
+    }
+}
