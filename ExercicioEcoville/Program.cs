9eceacf21ef2d9718a8a56e7098b3f9db75df659
Robson Adorno <rferreira@scopus.com.br>
2018-03-06 08:06:39 -0300

    Aplicação simples, no Console com a linguagem C#

---
+++ ExercicioEcoville/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExercicioEcoville
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
